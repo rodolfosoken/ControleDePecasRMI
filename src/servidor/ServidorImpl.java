c34a933329a679920fd9483bd6ac91852306f662
Rodolfo Soken <rodolfosoken@gmail.com>
2018-07-10 05:25:49 -0300

    Registro do Servidor ok

--- src/servidor/ServidorImpl.java
+++ src/servidor/ServidorImpl.java
@@ -1,9 +1,12 @@
 package servidor;
 
 import java.rmi.AccessException;
+import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.Map;
 
 import interfaces.Part;
@@ -51,18 +54,37 @@ public class ServidorImpl implements Servidor, PartRepository, Part{
 	public ServidorImpl(String servidorNome) {
 		this.servidorNome = servidorNome;
 		this.repository = new PartRepositoryImpl("repos_"+servidorNome);
+		
+		try {
+			Servidor stub = (Servidor) UnicastRemoteObject.exportObject(this,0);
+			reg = LocateRegistry.getRegistry();
+			reg.bind(servidorNome, stub);
+			System.out.println("Servidor "+servidorNome+" iniciado.");
+		} catch (RemoteException e) {
+			System.out.println("Erro ao iniciar servidor: "+servidorNome+" verifique o rmiregistry.");
+			reg = null;
+			System.out.println(e.getLocalizedMessage());;
+		} catch (AlreadyBoundException e) {
+			System.out.println("Stub do servidor: "+servidorNome+" já registrado.");
+			e.printStackTrace();
+		}
 	}
 	
 	
 	public void shutdown() {
 		try {
-			reg.unbind(this.servidorNome);
+			if(reg!=null) {
+				reg.unbind(this.servidorNome);
+				System.out.println("Servidor: "+servidorNome+" finalizado.");
+			}
 		} catch (AccessException e) {
 			System.out.println("Erro ao finalizar o servidor: "+this.servidorNome);
 			e.printStackTrace();
 		} catch (RemoteException e) {
+			System.out.println("Erro ao finalizar o servidor: "+this.servidorNome);
 			e.printStackTrace();
 		} catch (NotBoundException e) {
+			System.out.println("Erro ao finalizar o servidor: "+this.servidorNome);
 			e.printStackTrace();
 		}
 	}
